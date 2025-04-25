47a1109b787b15f289aa59c6e46b751d0809966f
Alfej Mansuri <alfejm@maqsoftware.com>
2025-04-25 15:37:08 +0530

    Threading lock applied on singletone instance creation

--- System design/design patterns/singleton.py
+++ System design/design patterns/singleton.py
@@ -9,24 +9,54 @@
 # A metaclass can be used to customize class creation, modify class attributes, and enforce constraints on class definitions
 # A metaclass is defined by inheriting from the type class. It can override methods like __new__ and __init__ to customize class creation and initialization.
 
+
+# without threading implementation
+# class SingletonMeta(type):
+    
+#     _instances={}
+    
+#     def __call__(cls, *args, **kwargs):
+#         if cls not in cls._instances:
+#             instance = super().__call__(*args, **kwargs)
+#             cls._instances[cls] = instance
+#         return cls._instances[cls]
+
+
+from threading import Lock,Thread
 class SingletonMeta(type):
     
     _instances={}
+    _lock : Lock = Lock()
     
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super.__call__(cls, *args, **kwargs)
-            cls._instances[cls] = instance
+        with cls._lock:
+            # Check if an instance already exists
+            if cls not in cls._instances:
+                # Create a new instance and store it in the dictionary
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
         return cls._instances[cls]
     
     
 class Singleton(metaclass=SingletonMeta):
-    pass
+    
+    def __init__(self,value):
+        self.value = value
+        
+    def get_value(self):
+        return self.value
+    
+    def get_id(self):
+        return id(self)
 
+def test(value):
+    singleton = Singleton(value)
+    print(f"Singleton value: {singleton.get_value()}")
+    print(f"Singleton ID: {singleton.get_id()}")
 
 if __name__ == "__main__":
-    s1 = Singleton()
-    s2 = Singleton()
+    process1 = Thread(target=test, args=("First Object",))
+    process2 = Thread(target=test, args=("Second Object",))
     
-    print(s1 is s2)  # True, both variables point to the same instance
-    print(id(s1), id(s2))  # Both IDs will be the same, confirming they are the same instance
+    process1.start()
+    process2.start() 
