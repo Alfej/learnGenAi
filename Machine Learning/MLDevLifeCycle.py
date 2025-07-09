999270a5f8c602f974ad83003f37d6095ae0fa83
Alfej <alfejmansuri136@gmail.com>
2025-07-09 14:49:01 +0530

    MLDLC

---
+++ Machine Learning/MLDevLifeCycle.py
@@ -0,0 +1,16 @@
+# Machine Learning Development Life Cycle
+# 1. Problem Definition
+# 2. Data Collection
+# 3. Data Preprocessing
+    #    - Data Cleaning
+    #    - Data Transformation
+    #    - Data Normalization/Standardization
+    #    - Data Splitting (Train/Test/Validation)
+# 4. Exploratory Data Analysis (EDA)
+    #    - Data Visualization
+    #    - Univariate and Multivariate Analysis
+    #    - Outlier Detection
+# 5. Feature Engineering and Selection
+# 6. Model training, Evaluation and selection
+# 7. model Deployment
+# 8. Model Monitoring and Maintenance
