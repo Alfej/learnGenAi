62f9a92c040079db38f8cc8d65267440dc6e0a07
Alfej <alfejmansuri136@gmail.com>
2025-07-09 15:09:41 +0530

    Tensor

---
+++ Machine Learning/Introduction/Tensor.py
@@ -0,0 +1,12 @@
+# Tensor: It is a Data Structure
+# Tensors are multi-dimensional arrays that can be used to represent data in machine learning models.
+
+
+# 0D Tensor: A scalar value, such as a single number.
+# 1D Tensor: A vector, which is a list of numbers.
+# 2D Tensor: A matrix, which is a table of numbers with rows and columns.
+# 3D Tensor: A cube of numbers, which can be thought of as a stack of matrices.
+
+
+# no of axis = Rank = number of dimensions
+
